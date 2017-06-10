b211cf7ab2a31fb0d49c6387b2a26c063836ddbd
reza <reza@vixre.co.uk>
2017-06-10 12:57:05 +0100

    restructured into something a bit more orderly and have file browser open

--- wppublisher/push/server.py
+++ wppublisher/push/server.py
@@ -1,13 +1,15 @@
 import digitalocean
+import paramiko
 
 class digital_ocean():
     def __init__(self):
-        self.test = 'none';
+        self.api_key = "eff3d2d39c9a7cd088a0be8f6d8859361057da4c7f21d5540e0456c9ec3fa726"
 
-    def create(self):
-        apiKey = "eff3d2d39c9a7cd088a0be8f6d8859361057da4c7f21d5540e0456c9ec3fa726"
-
-        manager = digitalocean.Manager(token=apiKey)
+    '''
+    Creates the server instance
+    '''
+    def spin_up(self):
+        manager = digitalocean.Manager(token=self.api_key)
         my_droplets = manager.get_all_droplets()
         print(my_droplets)
         with open('user_data.txt', 'r') as myfile:
@@ -15,9 +17,27 @@ class digital_ocean():
 
         print(user_data)
 
-        droplet = digitalocean.Droplet(token=apiKey,
+        instance = digitalocean.Droplet(token=self.api_key,
                                        name='Example1253',
                                        region='nyc2',  # New York 2
                                        image='ubuntu-14-04-x64',  # Ubuntu 14.04 x64
                                        size_slug='512mb',  # 512MB
                                        user_data=user_data)
+
+        instance.create()
+
+    '''
+    Additional configuration to prepare the site to host wp
+    '''
+    def configuration(self):
+        self.spin_up()
+
+        client = paramiko.SSHClient()
+        client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
+        client.connect('162.243.17.149', username='demo', allow_agent=True)
+        stdin, stdout, stderr = client.exec_command('ls /')
+        print(stdout.readlines())
+
+    def initialize(self):
+        self.configuration()
+
--- wppublisher/wp-publisher.py
+++ wppublisher/wp-publisher.py
@@ -1,51 +1,13 @@
 import sys
-import paramiko
-
-from PyQt5 import QtCore, QtGui, QtWidgets
-from PyQt5.QtWidgets import QWidget, QPushButton, QApplication
-from PyQt5.QtCore import QCoreApplication
+from PyQt5.QtWidgets import (QMainWindow, QTextEdit,
+                             QAction, QFileDialog, QApplication)
+from PyQt5.QtGui import QIcon
 
 from wppublisher.ui import gui
-from wppublisher.push import server
-
-def b1_clicked():
-   print ("Button 1 clicked")
-
-def selectFile():
-    fileDialog = QtGui.QFileDialog()
-    fileDialog.show()
+#from wppublisher.push import server
 
 
-def main():
-    app = QtWidgets.QApplication(sys.argv)
-    main_window = QtWidgets.QMainWindow()
-    ui = gui.Ui_MainWindow()
-    ui.setupUi(main_window)
-
-    vps = server.digital_ocean()
-    # vps.create()
-
-    # ui.publish_start.clicked(ui.get(), b1_clicked)
-
-    # QtCore.QObject.connect(main_window_dialog, QtCore.SIGNAL("installation_start_button"), b1_clicked)
-
-    start_button = ui.installation_start_button
-    start_button.clicked.connect(b1_clicked)
-
-    initialize_file_selector = ui.installation_path_file_selector
-    initialize_file_selector.clicked.connect(selectFile)
-
-    main_window.show()
+if __name__ == '__main__':
+    app = QApplication(sys.argv)
+    ex = gui.wp_gui()
     sys.exit(app.exec_())
-    # droplet.create()
-
-    # client = paramiko.SSHClient()
-    # client.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-    # client.connect('162.243.17.149', username='demo', allow_agent=True)
-    # stdin, stdout, stderr = client.exec_command('ls /')
-    # print(stdout.readlines())
-    # sys.exit(app.exec_())
-
-
-if __name__ == "__main__":
-    main()
